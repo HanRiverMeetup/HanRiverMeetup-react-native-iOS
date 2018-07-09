a885476655121e3df7f710333a9e75c9cc9d415d
HanYoungsoo <gglife333@gmail.com>
2018-07-10 01:33:00 +0900

    import react-native-maps

--- src/screens/Join.js
+++ src/screens/Join.js
@@ -2,6 +2,11 @@ import React, { Component } from "react"
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from "react-native"
 
 export default class Home extends Component {
+  static navigationOptions = options => {
+    return {
+      title: "같이놀강"
+    }
+  }
   render() {
     return (
       <View style={styles.container}>

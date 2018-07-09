79b43e8d8c067d8901082a2fab80ac8bf5d30bf8
Youngsu Han <amazingmobdev@gmail.com>
2018-07-10 01:34:07 +0900

    import react-native-maps (#1)

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react"
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from "react-native"
+import MapView from "react-native-maps"
 
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Map</Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
       </View>
     )
   }
@@ -16,5 +25,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   }
 })

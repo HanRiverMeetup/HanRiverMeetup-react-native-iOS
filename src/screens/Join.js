8c837c1695d261f1416d6a18c6d02e4fd2c69063
HanYoungsoo <gglife333@gmail.com>
2018-07-10 20:47:37 +0900

    added nanum font

--- src/screens/Join.js
+++ src/screens/Join.js
@@ -1,25 +1,35 @@
-import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native"
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default class Home extends Component {
   static navigationOptions = options => {
     return {
-      title: "같이놀강"
-    }
-  }
+      title: '같이놀강',
+    };
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Join</Text>
+        <Text style={styles.text}>동해물과 백두산이 마르고 닳도록!</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'NanumSquareR',
+    fontSize: 22,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 32,
+    letterSpacing: 0,
+    textAlign: 'left',
+    color: '#333333',
+  },
+});

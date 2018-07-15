87465ed97fb3d8c522509864b9703b4207ea2308
HanYoungsoo <gglife333@gmail.com>
2018-07-16 02:26:36 +0900

    added images

---
+++ assets/images/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable global-require */
+const images = {
+  logo_icon: require('./app_logo_1.png'),
+  app_title: require('./app_logo_2.png'),
+  facebook_logo: require('./facebook_logo.png'),
+  chicken_icon: require('./chicken_icon.png'),
+  bycycle_icon: require('./bycycle_icon.png'),
+  duck_icon: require('./duck_icon.png'),
+  camp_icon: require('./camp_icon.png'),
+  camera_icon: require('./camera_icon.png'),
+  etc_icon: require('./etc_icon.png'),
+  rectangle: require('./rectangle.png'),
+  bubble: require('./bubble.png'),
+  bt_back: require('./bt_back.png'),
+  tent_icon: require('./tent_icon.png'),
+  chicken_icon_w: require('./chicken_icon_w.png'),
+  bt_plus: require('./bt_plus.png'),
+};
+/* eslint-enable global-require */
+
+export default images;

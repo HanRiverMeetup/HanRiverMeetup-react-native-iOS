1aa5f06e05fb94837e284130c2363bde96ec1c56
HanYoungsoo <gglife333@gmail.com>
2018-07-21 03:58:34 +0900

    made a BaseText

---
+++ src/components/BaseText.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Base = styled.Text`
+  font-family: NanumSquareR;
+  font-size: 18;
+  color: white;
+`;
+
+const BaseText = ({ style, children }) => <Base style={style}>{children}</Base>;
+
+BaseText.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  style: PropTypes.oneOfType(PropTypes.arrayOf(PropTypes.number), PropTypes.number),
+};
+
+BaseText.defaultProps = {
+  style: {},
+};
+
+export default BaseText;

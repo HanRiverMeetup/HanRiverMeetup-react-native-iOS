ee9a2e7f8e7600cf8cd297bd138962072c06eeba
Youngsoo Han <amazingmobdev@gmail.com>
2018-07-30 22:31:14 +0900

    configured BaseButton

--- src/components/BaseButton.js
+++ src/components/BaseButton.js
@@ -7,14 +7,13 @@ const ButtonContainer = styled.TouchableHighlight`
   align-items: center;
 `;
 
-const BaseButton = props => (
-  <ButtonContainer {...props} underlayColor="rgba(240,256,256,0.7)">
-    {props.children}
-  </ButtonContainer>
-);
+const BaseButton = props => <ButtonContainer {...props}>{props.children}</ButtonContainer>;
 
 BaseButton.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  underlayColor: PropTypes.string,
 };
+
+BaseButton.defaultProps = { underlayColor: 'rgba(240,256,256,0.7)' };
 
 export default BaseButton;

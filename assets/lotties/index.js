91c584953ca49026a22a66e8bcbd1462c1911bfe
Youngsoo Han <amazingmobdev@gmail.com>
2018-08-06 21:40:01 +0900

    added lottie animation

---
+++ assets/lotties/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable global-require */
+const Lotties = {
+  splash: require('./data.json'),
+};
+/* eslint-disable global-require */
+export default Lotties;
--- src/screens/Launch.js
+++ src/screens/Launch.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { AccessToken } from 'react-native-fbsdk';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Animated, Easing } from 'react-native';
 import PropTypes from 'prop-types';
+import LottieView from 'lottie-react-native';
+import Lotties from '@lottie';
 
 export default class Launch extends Component {
   static propTypes = {
@@ -11,20 +13,45 @@ export default class Launch extends Component {
     }).isRequired,
   };
 
-  componentDidMount() {
-    AccessToken.getCurrentAccessToken().then(data => {
-      if (_.isEmpty(data)) {
-        this.props.navigation.navigate('Login');
-        return;
-      }
-      this.props.navigation.navigate('App');
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: new Animated.Value(0),
+    };
   }
+
+  componentDidMount() {
+    this.startAnimation();
+
+    setTimeout(() => {
+      AccessToken.getCurrentAccessToken().then(data => {
+        if (_.isEmpty(data)) {
+          this.props.navigation.navigate('Login');
+          return;
+        }
+        this.props.navigation.navigate('App');
+      });
+    }, 2100);
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.progress, {
+      toValue: 1,
+      duration: 2200,
+      easing: Easing.linear,
+    }).start();
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>LaunchScreen</Text>
+        <LottieView
+          ref={animation => {
+            this.animation = animation;
+          }}
+          source={Lotties.splash}
+          progress={this.state.progress}
+        />
       </View>
     );
   }

f37dffdc4032b376c0015bd897e22dd730557682
YoungsooHan <amazingmobdev@gmail.com>
2018-09-06 02:14:39 +0900

    implemented withLoading

--- src/HOC/WithLoading.js
+++ src/HOC/WithLoading.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import { Observer } from 'mobx-react';
 import FrameLoading from 'react-native-frame-loading';
-import PropTypes from 'prop-types';
 import hoistNonReactStatic from 'hoist-non-react-statics';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Images from '@assets';
+
+const LoadingImg = styled.Image`
+  width: 40;
+  height: 40;
+`;
+
+const VIEWS = [
+  <LoadingImg key={1} source={Images.loading_01} />,
+  <LoadingImg key={2} source={Images.loading_02} />,
+  <LoadingImg key={3} source={Images.loading_03} />,
+  <LoadingImg key={4} source={Images.loading_04} />,
+  <LoadingImg key={5} source={Images.loading_05} />,
+  <LoadingImg key={6} source={Images.loading_06} />,
+];
 
 const withLoading = WrappedComponent => {
   class WithLoading extends React.Component {
+    static propTypes = {
+      isLoading: PropTypes.bool.isRequired,
+    };
+
+    componentDidMount = () => {};
+
     render() {
+      const { isLoading } = this.props;
+      console.log('isLoading', isLoading);
       return (
         <React.Fragment>
-          <Observer>{() => console.log(this.props)}</Observer>
+          <Observer>
+            {() => (
+              <React.Fragment>
+                <WrappedComponent {...this.props} />
+                <FrameLoading
+                  animating={isLoading}
+                  views={VIEWS}
+                  duration={150}
+                  modalProps={{ transparent: true }}
+                  loadingContainerStyle={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flex: 1,
+                    backgroundColor: 'rgba(49,49,49,0.6)',
+                  }}
+                />
+              </React.Fragment>
+            )}
+          </Observer>
         </React.Fragment>
       );
     }
@@ -18,6 +61,10 @@ const withLoading = WrappedComponent => {
   hoistNonReactStatic(WithLoading, WrappedComponent);
 
   return WithLoading;
+};
+
+withLoading.defaultProps = {
+  isLoading: PropTypes.bool.isRequired,
 };
 
 export default withLoading;

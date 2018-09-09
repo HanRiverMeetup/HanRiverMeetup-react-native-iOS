4803df4854727d8e4b6c3c2cd0f4bfe5c634b923
YoungsooHan <amazingmobdev@gmail.com>
2018-09-09 20:34:02 +0900

    add fetchRoomsBySeq to store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,20 @@
 import { types } from 'mobx-state-tree';
 
 import UserStore from './UserStore';
+import RoomStore from './RoomStore';
 import serverInfo from '../configs';
 
 const ACCESS_ENDPOINT = `${serverInfo.url}/access`;
+const MEETING_HOST_ENDPOINT = `${serverInfo.url}/host/meeting`;
+const MEETINGS_HOST_ENDPOINT = `${serverInfo.url}/host/meetings`;
 
 const Store = types
   .model('Store', {
     userStore: types.optional(UserStore, UserStore.create()),
+    roomStore: types.optional(RoomStore, RoomStore.create()),
   })
   .views(self => {
-    const Fetch = async (method, url, params) => {
+    const Fetch = async (method, url, params = undefined) => {
       const headers = {
         'Content-Type': 'application/json',
       };
@@ -36,10 +40,13 @@ const Store = types
 
     const loginValidate = params => Fetch('POST', `${ACCESS_ENDPOINT}/loginValidate`, params);
     const registUser = params => Fetch('POST', `${ACCESS_ENDPOINT}/registUser`, params);
+    const fetchRoomsBySeq = params =>
+      Fetch('GET', `${MEETINGS_HOST_ENDPOINT}/week/${params.activity_seq}`);
 
     return {
       loginValidate,
       registUser,
+      fetchRoomsBySeq,
     };
   });
 

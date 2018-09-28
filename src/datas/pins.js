2ca757a7e3bb2e97253806f64c8200e5c7d5bfcc
Youngsoo Han <amazingmobdev@gmail.com>
2018-09-29 03:32:57 +0900

    datas

---
+++ src/datas/pins.js
@@ -0,0 +1,80 @@
+export const toilets = [
+  {
+    latitude: 37.534454,
+    longitude: 126.988834,
+  },
+  {
+    latitude: 37.543063,
+    longitude: 126.990143,
+  },
+  {
+    latitude: 37.534169,
+    longitude: 126.992371,
+  },
+  {
+    latitude: 37.550428,
+    longitude: 126.976212,
+  },
+  {
+    latitude: 37.545499,
+    longitude: 126.984006,
+  },
+  {
+    latitude: 37.523158,
+    longitude: 126.961087,
+  },
+  {
+    latitude: 37.531685,
+    longitude: 126.968651,
+  },
+];
+
+export const trashCans = [
+  {
+    latitude: 37.6002049,
+    longitude: 127.0732013,
+  },
+  {
+    latitude: 37.5812836,
+    longitude: 127.08012,
+  },
+  {
+    latitude: 37.5941269,
+    longitude: 127.1096252,
+  },
+  {
+    latitude: 37.6145471,
+    longitude: 127.1009378,
+  },
+  {
+    latitude: 37.6161311,
+    longitude: 127.0736929,
+  },
+];
+
+export const conveniencStores = [
+  {
+    latitude: 37.5358209,
+    longitude: 126.9436686,
+  },
+  {
+    latitude: 37.56028468,
+    longitude: 126.9012186,
+  },
+  {
+    latitude: 37.56227267,
+    longitude: 126.9154965,
+  },
+  {
+    latitude: 37.58523469,
+    longitude: 126.8839481,
+  },
+  {
+    latitude: 37.58261739,
+    longitude: 126.8837514,
+  },
+  {
+    latitude: 37.57620135,
+    longitude: 126.8864602,
+  },
+];
